5f1f8c07a5ede316ac9739deb632a0cc9c23d17c
mivige <130495226+mivige@users.noreply.github.com>
2023-10-07 18:11:15 +0200

    Create arduino.ino

---
+++ Code/arduino.ino
@@ -0,0 +1,36 @@
+// File to setup Arduino
+
+// Declaration of global variables
+float temp=0;
+float light=0;
+float carb=0;
+float mos=0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Reading five times and making an average from that readings
+  for(int i=0; i<5; i++)
+  {
+    temp+=analogRead(0);
+    light+=analogRead(1); //Range da 7 a 10
+    carb+=analogRead(2);
+    mos+=analogRead(5);
+  }
+  temp=temp/5;
+  light=light/5;
+  carb=carb/5;
+  mos=mos/5;
+
+  // Print results to the serial converting the values in the unit of measure that we'll be using
+  Serial.print(temp*(5/10.24));
+  Serial.print(",");
+  Serial.print(light);
+  Serial.print(",");
+  Serial.print(carb);
+  Serial.print(",");
+  Serial.println(mos);
+  delay(10000);
+}
