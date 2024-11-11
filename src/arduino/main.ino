e47caf3b984e5148ffaf999b0eff269bf84ccc7b
Triveri Samuele <samuele.triveri@protonmail.com>
2024-11-11 21:50:55 +0100

    Added comments about sensors and analogRead function

--- src/arduino/main.ino
+++ src/arduino/main.ino
@@ -1,7 +1,11 @@
-// Temperatura 20 - 25C
-// Luce >1000 Lux
-// Carbonio 800 - 1500 ppm
-// Umidità 300 - 700 
+/* The method used to read values from sensors
+ * and how to convert them in the desired 
+ * measurement unit depends on how you physically
+ * connect the sensors to the board for the former,
+ * and the instructions provided from your specific
+ * sensor's datasheet / from the manufacturer's website
+ * for the latter.
+*/
 
 float temp=0;
 float light=0;
@@ -15,27 +19,34 @@ void setup() {
 void loop() {
   for(int i=0; i<5; i++)
   {
+
+    /*
+    * Note that the argument of analogRead is
+    * the name / number of the board's analog to
+    * digital input pin, which may be different
+    * among boards.
+    */
+
     temp+=analogRead(0);
-    light+=analogRead(1); //Range da 7 a 10
+    light+=analogRead(1);
     carb+=analogRead(2);
     mos+=analogRead(5);
   }
+
   temp=temp/5;
   light=light/5;
   carb=carb/5;
   mos=mos/5;
 
-  //Serial.println("Temperatura: ");
   Serial.print(temp*(5/10.24));
   Serial.print(",");
-  //Serial.println("Luce: ");
+
   Serial.print(light);
   Serial.print(",");
-  //Serial.println("Carbonio: ");
+  
   Serial.print(carb);
   Serial.print(",");
-  //Serial.println("Umidità: ");
+  
   Serial.println(mos);
-  //Serial.println("----------");
   delay(10000);
 }
